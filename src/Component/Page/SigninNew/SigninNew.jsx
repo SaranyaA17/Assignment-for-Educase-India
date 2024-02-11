41a6636138c2aeb1b0d556f36017644245684bb9
SaranyaA17 <saranya17112002@gmail.com>
2024-02-11 13:13:14 +0530

    JS Form

--- src/Component/Page/SigninNew/SigninNew.jsx
+++ src/Component/Page/SigninNew/SigninNew.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./signinNew.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SigninNew = () => {
   const navigate = useNavigate();
